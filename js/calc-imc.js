89466c76e36c42e545193ea9a41fb90772221387
Mateus Queiroz <mateusqueirozdev@gmail.com>
2017-10-07 17:29:33 -0300

    Validação dos campos

--- js/calc-imc.js
+++ js/calc-imc.js
@@ -3,9 +3,18 @@ var pacientes = document.querySelectorAll(".paciente");
 for(let i = 0; i < pacientes.length; i++){
   var pesoValido = true;
   var alturaValida = true;
+  var tdPeso = pacientes[i].querySelector(".info-peso");
 
   if(pacientes[i].querySelector(".info-peso").textContent >= 300 || pacientes[i].querySelector(".info-peso").textContent <= 0){
       var pesoValido = false;
+      if(pacientes[i].querySelector(".info-imc")){
+          var tdImc = pacientes[i].querySelector(".info-imc");
+          var tdPeso = pacientes[i].querySelector(".info-peso");
+          tdPeso.classList.add("campo-invalido");
+          tdImc.textContent = "Peso inválido!";
+      } else {
+          alert("Campo inexistente!");
+      }
   } else {
       var pesoValido = true;
       var peso = parseInt(pacientes[i].querySelector(".info-peso").textContent);
@@ -13,6 +22,14 @@ for(let i = 0; i < pacientes.length; i++){
 
   if(pacientes[i].querySelector(".info-altura").textContent >= 3.50 || pacientes[i].querySelector(".info-altura").textContent <= 0){
       var alturaValida = false;
+      if(pacientes[i].querySelector(".info-imc")){
+          var tdImc = pacientes[i].querySelector(".info-imc");
+          var tdAltura = pacientes[i].querySelector(".info-altura");
+          tdAltura.classList.add("campo-invalido");
+          tdImc.textContent = "Altura inválida!";
+      } else {
+          alert("Campo inexistente!");
+      }
   } else {
       var alturaValida = true;
       var altura = parseFloat(pacientes[i].querySelector(".info-altura").textContent);
@@ -22,14 +39,7 @@ for(let i = 0; i < pacientes.length; i++){
       var resultado = peso / (altura * altura);
       if(pacientes[i].querySelector(".info-imc")){
           var tdImc = pacientes[i].querySelector(".info-imc");
-          tdImc.textContent = resultado.toString().substring(0,5);
-      } else {
-          alert("Campo inexistente!");
-      }
-  } else {
-      if(pacientes[i].querySelector(".info-imc")){
-          var tdImc = pacientes[i].querySelector(".info-imc");
-          tdImc.textContent = "Peso e/ou Altura inválidos!";
+          tdImc.textContent = resultado.toFixed(2);
       } else {
           alert("Campo inexistente!");
       }
