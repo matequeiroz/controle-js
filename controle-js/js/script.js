ae5d75c6547b3392577d875f01ef6b06fb50e214
Mateus Queiroz <mateusqueirozdev@gmail.com>
2017-10-07 15:47:41 -0300

    deletando a pasta

--- controle-js/js/script.js
+++
@@ -1,7 +0,0 @@
-if (document.querySelector("#titulo-principal")) {
-	var titulo = document.querySelector("h1");
-	titulo.textContent = "Mateus Nutrição";
-} else {
-	alert("Nenhum elemento localizado.");
-}
-
